b68e385c79dacafdc9ba8188438d755fc6338e15
reximy-rhay <111143140+reximy-rhay@users.noreply.github.com>
2022-08-13 07:01:42 -0700

    Update login.php

--- login.php
+++ login.php
@@ -9,18 +9,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="./login.css">
-   
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="	https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 
-   
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <!------ Include the above in your HEAD tag ---------->
 
-   
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
 </head>
 
 <body style="background-color: white;">
@@ -31,23 +31,21 @@
                 <div class="navs">
                     <nav class="navbar bg-primary fixed-top ">
                         <div class="container-fluid">
-                            <a class="navbar-brand" style="color: black; font-weight:900;" href="./index.html">Wikibhu</a>
+                            <a class="navbar-brand" style="color: black; font-weight:900;" href="#">Practice Questions</a>
                             <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
                         <span class="navbar-toggler-icon"></span>
                       </button>
                             <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
                                 <div class="offcanvas-header">
-                                    <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
+                                    <h5 class="offcanvas-title" id="offcanvasNavbarLabel">wikibhu</h5>
                                     <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                                 </div>
                                 <div class="offcanvas-body">
                                     <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
                                         <li class="nav-item">
-                                            <a class="nav-link active" aria-current="page" href="./index.html"><i class="uil uil-home"></i>Home</a>
+                                            <a class="nav-link active" aria-current="page" href="./index.php"><i class="uil uil-home"></i>Home</a>
                                         </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link" href="#"><i class="uil uil-book-alt"></i>Take a test</a>
-                                        </li>
+                                       
                                         <li class="nav-item">
                                             <a class="nav-link" href="./login.php"><i class="uil uil-book-alt"></i>Login</a>
                                         </li>
@@ -64,19 +62,19 @@
                                                 <li>
                                                     <hr class="dropdown-divider">
                                                 </li>
-                                                <li><a class="dropdown-item" href="../project3/OnePage/index.html">Logout</a></li>
+                                                <li><a class="dropdown-item" href="./bhu.html">Logout</a></li>
                                             </ul>
                                         </li>
                                     </ul>
                                     <form class="d-flex" role="search">
+                                   
                                         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
                                         <button class="btn btn-outline-success" type="submit">Search</button>
-                                        </div>
-                                     </form>
+                                    </form>
+                                </div>
                             </div>
                         </div>
-                    </nav>
-                </div>                                
+                    </nav>                               
                                 
                 <section class="vh-100">
   <div class="container-fluid h-custom">
